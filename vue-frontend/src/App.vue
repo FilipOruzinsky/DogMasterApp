dfec23856c4c4b7c11004e0cdbe79290c96abea4
Peter Szathmáry <pet.szathmary@gmail.com>
2025-10-15 20:05:38 +0200

    Add `package-lock.json` for Vue frontend dependencies.
    - Added a comprehensive `package-lock.json` to manage dependency versions for the Vue frontend project.
    - Ensured inclusion of required libraries such as `vue`, `vue-router`, `keycloak-js`, and various dev dependencies like `vite`, `eslint`, and `typescript`.

--- vue-frontend/src/App.vue
+++ vue-frontend/src/App.vue
@@ -1,4 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import keycloak from '@/keycloak.ts'
+
+const logout = () => {
+  keycloak.logout({ redirectUri: window.location.origin })
+}
+</script>
 
 <template>
   <h1>You did it!</h1>
@@ -6,6 +12,7 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <button @click="logout">Logout</button>
 </template>
 
 <style scoped></style>
