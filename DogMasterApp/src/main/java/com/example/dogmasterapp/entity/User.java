34e6954ed0f672b7017b24c21b3d1d9c6208eab7
Peter Szathmáry <pet.szathmary@gmail.com>
2025-10-22 19:31:54 +0200

    Update Hibernate config to `validate` mode and add column mappings in User entity.

--- DogMasterApp/src/main/java/com/example/dogmasterapp/entity/User.java
+++ DogMasterApp/src/main/java/com/example/dogmasterapp/entity/User.java
@@ -15,11 +15,16 @@ import java.util.List;
 
 public class User {
     @Id
+    @Column(name = "userid")
     public String userID;
+    @Column(name = "user_name")
     public String userName;
+    @Column(name = "first_name")
     public String firstName;
+    @Column(name = "last_name")
     public String lastName;
     public String address;
+    @Column(name = "phone_number")
     public String phoneNumber;
     public String email;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
