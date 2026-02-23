9d6d433b7ccc54c64dbca68fc63661567191d8d3
Peter Szathmáry <peter.szathmary@proton.me>
2026-02-23 10:42:29 +0100

    Enhance `AnswerService` with specific exceptions and streamlined error handling logic

---
+++ DogMasterApp/src/main/java/com/example/dogmasterapp/exception/VotingForOwnAnswerException.java
@@ -0,0 +1,7 @@
+package com.example.dogmasterapp.exception;
+
+public class VotingForOwnAnswerException extends RuntimeException {
+    public VotingForOwnAnswerException(String message) {
+        super(message);
+    }
+}
